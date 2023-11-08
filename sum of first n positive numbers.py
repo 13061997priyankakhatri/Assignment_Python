4669afc21e4312524e6970251cc3e2cadcc13007
13061997priyankakhatri <137032972+13061997priyankakhatri@users.noreply.github.com>
2023-11-08 11:38:59 +0530

    Update sum of first n positive numbers.py

--- sum of first n positive numbers.py
+++ sum of first n positive numbers.py
@@ -4,14 +4,14 @@ first n positive integers.'''
 '''Prompt the user for input and convert
 it to an integer.'''
 
-n = int(input("Input a number: "))
+p = int(input("Input a number: "))
 
 '''Calculate the sum of the first 'n'
 positive integers using the formula.'''
 
-sum_num = (n * (n + 1)) / 2
+sum_num = (p * (p + 1)) / 2
 
 '''Print the result, indicating the sum
 of the first 'n' positive integers.'''
 
-print("Sum of the first", n, "positive integers:", sum_num)
+print("Sum of the first", p, "positive integers:", sum_num)
