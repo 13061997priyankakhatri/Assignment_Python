c45f6c53013152bdc2855ae3c742bb3e9820843f
13061997priyankakhatri <137032972+13061997priyankakhatri@users.noreply.github.com>
2023-11-28 17:51:37 +0530

    Update Dictinaries.py

--- Pyhton_Task_Programs/Dictinaries.py
+++ Pyhton_Task_Programs/Dictinaries.py
@@ -1,14 +1,14 @@
+print()
 Dict_1 ={"name" : "Priyanka","age" : 26, "canvote" : True}
 print(Dict_1)
 
-
-print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-
 print()
-Dict2 = {}
+print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+print()
 
+Dict2 = {}
 alnum = input("Enter email : ")
-for p in range(2):
+for p in range(15):
     Dict2[alnum[p]]= p
 print("Email is:", Dict2)
 
@@ -20,5 +20,24 @@ for u in range(0,8):
 print("Password is:", Dict3)
 
 print()
+print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+print()
 
- 
+Tops = {}
+
+name = input("Enter student name : ")
+id = input("Enter student id : ")
+
+Tops['Student'] = {'name':name,'id':id}
+
+course_name = input("Enter course name : ")
+fees = int(input("Enter course fees : "))
+
+Tops['Courses'] = {'course_name':course_name,'fees':fees}
+
+name = input("Enter faculty name : ")
+id = input("Enter faculty id : ")
+
+Tops['Feculty'] = {'name':name,'id':id}
+
+print(Tops)
