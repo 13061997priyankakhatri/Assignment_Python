ed31707846d5b69fcd9ad6955d30b0cadfb7fdb5
13061997priyankakhatri <naynakhatri999@gmail.com>
2023-11-28 17:40:42 +0530

    merge other

--- Pyhton_Task_Programs/Dictinaries.py
+++ Pyhton_Task_Programs/Dictinaries.py
@@ -5,10 +5,11 @@ print(Dict_1)
 print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 
 print()
+'''
 Dict2 = {}
 
 alnum = input("Enter email : ")
-for p in range(2):
+for p in range(7):
     Dict2[alnum[p]]= p
 print("Email is:", Dict2)
 
@@ -18,7 +19,30 @@ alpha = input("Enter password :")
 for u in range(0,8):
     Dict3[alpha[u]]= u 
 print("Password is:", Dict3)
+'''
+
 
 print()
+
+print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+
+Tops = {}
+
+name = input("Enter student name : ")
+id = input("Enter student id : ")
+
+Tops['Student'] = {'name':name,'id':id}
+
+course_name = input("Enter course name : ")
+fees = int(input("Enter course fees : "))
+
+Tops['Courses'] = {'course_name':course_name,'fees':fees}
+
+name = input("Enter faculty name : ")
+id = input("Enter faculty id : ")
+
+Tops['Feculty'] = {'name':name,'id':id}
+
+print(Tops)
 
  
